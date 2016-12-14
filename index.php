40beab4501055495a5709e8b61aef34d3a739664
Tomek <tomasz@tomaka.eu>
2016-12-14 18:53:11 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo 'Index will be here!'; ?>
+<html>
+    <body>
+        <form action="index.php" method="get">
+            <input type="submit" name="service" value="apache">
+            <input type="submit" name="service" value="mysql">
+            <input type="submit" name="service" value="php">
+			<br>
+            <input type="submit" name="service" value="apache_uninstall">
+            <input type="submit" name="service" value="mysql_uninstall">
+            <input type="submit" name="service" value="php_uninstall">
+        </form>
+    </body>
+</html>
+<?php
+
+
+
+    if(isset($_GET['service'])) {
+        echo "Installing ".$_GET['service'];
+		$output = system("sudo ./scripts/".$_GET['service'].".sh");
+		//$output = shell_exec("pwd");
+		print_r("<br>".$output);
+    }
+
+
+?>
