ee5d138b3a56683497ff4081bcb83bf7d69c376f
Tomek <tomasz@tomaka.eu>
2017-01-02 12:40:58 +0100

    Update bash scripts

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -14,4 +14,18 @@ apt-get update
 # install apache2
 apt-get -y install apache2
 
+#FROM PHP system("sudo ./scripts/apache.sh Timeout KeepAlive MaxKeepAliveRequest KeepAliveTimeout User Group HostnameLookups ErrorLog LogLevel Listen");
+#                                          $1      $2        $3                  $4               $5   $6    $7              $8       $9       $10
 
+sed -i 's/^Timeout .*/Timeout      = '$1'/' /etc/apache2/apache2.conf
+sed -i 's/^KeepAlive .*/KeepAlive      = '$2'/' /etc/apache2/apache2.conf
+sed -i 's/^MaxKeepAliveRequest .*/MaxKeepAliveRequest      = '$3'/' /etc/apache2/apache2.conf
+sed -i 's/^KeepAliveTimeout .*/KeepAliveTimeout      = '$4'/' /etc/apache2/apache2.conf
+sed -i 's/^User .*/User      = '$5'/' /etc/apache2/apache2.conf
+sed -i 's/^Group .*/Group      = '$6'/' /etc/apache2/apache2.conf
+sed -i 's/^HostnameLookups .*/HostnameLookups      = '$7'/' /etc/apache2/apache2.conf
+sed -i 's/^ErrorLog .*/ErrorLog      = '$8'/' /etc/apache2/apache2.conf
+sed -i 's/^LogLevel .*/LogLevel      = '$9'/' /etc/apache2/apache2.conf
+sed -i 's/^Listen .*/Listen      = '$10'/' /etc/apache2/apache2.conf
+
+service apache2 start
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-printf "first 1 is "$0
+printf "first 1 is "$1
 printf "first 2 is "$2
 printf "first 3 is "$3
